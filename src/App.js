eadc94fa28285e03be7288eb030785383f2e99e5
pearlmk <muthumk1929@gmail.com>
2023-07-04 00:57:35 -0700

    Todo-list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import Inputarea from './components/Inputarea';
+import TodoItem from './components/TodoItem';
 
 function App() {
+  const [items,setItem] = useState([])
+
+  const addItems = (inputText) =>{
+      setItem((prevItems)=>{
+        return [...prevItems,inputText]
+      })
+  };
+  const deleteItem = (id) =>{
+    setItem((prevItems) =>{
+      return prevItems.filter((item,index)=>{
+         return index!==id;
+      })
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+         <div className='heading'>  
+          <h3>To-Do List</h3>
+         </div>
+         <Inputarea addItems={addItems} />
+         <div>
+          <ul>
+            { items.map((item,index)=>{
+              return <TodoItem text={item} key={index} deleteItem={deleteItem} id={index} />
+            })}
+          </ul>
+         </div>
     </div>
+
   );
 }
 
